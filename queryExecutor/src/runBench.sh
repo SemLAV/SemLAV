57e13bcef5da36cde2d976f6dc4300f11878f2ca
gmontoya <gabrielamontoyac@gmail.com>
2015-04-22 17:39:51 +0200

    fix

--- queryExecutor/src/runBench.sh
+++ queryExecutor/src/runBench.sh
@@ -4,9 +4,14 @@ CONFIG_FILE="configD.properties.base"
 TARGET_KEY="nbWorker"
 
 for i in $(seq 0 5);do
-        $i = $i*5
-        sed -i -e '/$i =/ s/= .*/= $REPLACEMENT_VALUE/' $CONFIG_FILE
-        sh runBerlinSemlav.sh
-        java processResultsSemLAV /home/semlav/semLAV/expfiles/berlinOutput/TenMillions/510views 510views output /home/montoya/semLAV/code/expfiles/berlinData/TenMil$
+        i=$(($i*5))
+	if [ $i -eq 0 ]
+	then
+		i=1
+	fi
+        #sed -i -e '/$i =/ s/= .*/= $i/' $CONFIG_FILE
+        sed -i "s#nbWorker=.*#nbWorker=$i#" $CONFIG_FILE
+	sh runBerlinSemLAV.sh
+        java processResultsSemLAV /home/semlav/semLAV/expfiles/berlinOutput/TenMillions/510views 510views output /home/montoya/semLAV/code/expfiles/berlinData/TenMillions/answersSize /home/semlav/semLAV/results
         echo $i
 done
